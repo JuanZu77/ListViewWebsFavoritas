60258bbbb272827b970506001a8734729fd05280
Juan! <101385835+JuanZu77@users.noreply.github.com>
2022-03-31 00:39:54 -0300

    Update AdminSOLiteOpenHelper.java

--- src/com/example/listviewwebfavoritas/AdminSOLiteOpenHelper.java
+++ src/com/example/listviewwebfavoritas/AdminSOLiteOpenHelper.java
@@ -5,8 +5,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-
-
 public class AdminSOLiteOpenHelper extends SQLiteOpenHelper {
 
 	public AdminSOLiteOpenHelper(Context context, String name,
@@ -14,23 +12,17 @@ public class AdminSOLiteOpenHelper extends SQLiteOpenHelper {
 		super(context, name, factory, version);
 		// TODO Auto-generated constructor stub
 	}
-
-	
 	
 	@Override //--> OnCreate
 	public void onCreate(SQLiteDatabase arg0) {
 		// TODO Auto-generated method stub
 		
-		arg0.execSQL("create table web (descripcion text)");
-			
-
+		arg0.execSQL("create table web (descripcion text)");			
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
 		// TODO Auto-generated method stub
 
-	}
-	
-	
+	}	
 }
